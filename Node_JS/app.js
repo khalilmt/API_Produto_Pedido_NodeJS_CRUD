5dbbaf9134cba9efcf65311e3917d2e70423d7a9
khalilmt <kalilmteixeira@gmail.com>
2022-12-28 23:23:37 -0300

    Instalação do nodemon e morgan, tratamento de erros basico.

--- Node_JS/app.js
+++ Node_JS/app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 
 const rotaPedidos = require('./routes/pedidos');
 const rotaProdutos = require('./routes/produtos');
 
+app.use(morgan('dev'));
+
 app.use('/pedidos', rotaPedidos);
 app.use('/pedidos', rotaProdutos);
+
+app.use((req, res, next) => {
+    const erro = new Error('Nao encontrado');
+    erro.status = 404;
+    next(erro);
+
+});
+
+app.use((erro, req, res, next) => {
+    res.status(erro.status || 500);
+    return res.send({
+        erro: {
+            mensagem: erro.message
+        }
+    });
+});
 
 module.exports = app;
