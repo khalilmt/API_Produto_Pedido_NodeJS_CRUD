1a5015b47a71c1784812558f0a822b38634eba13
khalilmt <kalilmteixeira@gmail.com>
2022-12-30 12:51:21 -0300

    Ajuste rota no app.js

--- Node_JS/app.js
+++ Node_JS/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false})); //apenas dados simples
 app.use(bodyParser.json()); // json apenas como entrada no bodyParser
 
 app.use('/pedidos', rotaPedidos);
-app.use('/pedidos', rotaProdutos);
+app.use('/produtos', rotaProdutos);
 
 app.use((req, res, next) => {
     const erro = new Error('Nao encontrado');
