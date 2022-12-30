7f8f1a2cd4505fdc5cb71110a08d104d3b51b69e
khalilmt <kalilmteixeira@gmail.com>
2022-12-29 22:20:45 -0300

    ajustes

--- Node_JS/routes/produtos.js
+++ Node_JS/routes/produtos.js
@@ -1,1 +1,32 @@
 const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Usando GET dentro da rota de Produtos'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Usando POST dentro da rota de Produtos'
+    });
+});
+
+router.get('/:id_produto', (req, res, next) => {
+    const id = req.params.id_produto
+
+    if (id === 'unico') {
+        res.status(200).send({
+            mensagem: 'ID Unico',
+            id: id
+        });
+    }else{
+        res.status(200).send({
+            mensagem: 'Você passou um ID'
+        });
+    }
+});
+
+
+module.exports = router;
