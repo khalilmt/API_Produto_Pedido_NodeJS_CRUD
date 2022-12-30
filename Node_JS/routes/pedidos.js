a153834133d63bbaa36e393ef15c4268c2df42ff
khalilmt <kalilmteixeira@gmail.com>
2022-12-30 13:09:44 -0300

    Ajuste rota no app.js

---
+++ Node_JS/routes/pedidos.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+// RETORNA TODOS OS PEDIDOS
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        mensagem: 'Retorna um pedido'
+
+    });
+});
+
+// INSERE UM PEDIDO
+router.post('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Insere um pedido'
+    });
+});
+
+//  RETORNA OS DADOS DE UM PEDIDO
+router.get('/id_pedido', (req, res, next) => {
+    const id = req.params.id_pedido
+
+    res.status(200).send({
+        mensagem: 'Detalhes do pedido',
+        id_pedido: id
+    });
+});
+
+// EXCLUI UM PEDIDO
+router.delete('/', (req, res, next) => {
+    res.status(201).send({
+        mensagem: 'Deleta um pedido'
+    });
+});
+
+module.exports = router;
